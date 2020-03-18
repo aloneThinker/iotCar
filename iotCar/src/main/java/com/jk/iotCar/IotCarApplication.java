218e97af04cb7f909b15ace4fd6514b9a7636441
ZJK <JKMan0512@163.com>
2020-03-19 01:43:27 +0800

    智慧汽车

---
+++ iotCar/src/main/java/com/jk/iotCar/IotCarApplication.java
@@ -0,0 +1,13 @@
+package com.jk.iotCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotCarApplication.class,args);
+        System.out.println("hello word");
+    }
+}
